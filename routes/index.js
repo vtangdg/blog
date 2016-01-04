0a995a85879d82ca8e8e2f66860ad217bbd8c3a4
vtandg <stillwaterstang@163.com>
2016-01-04 23:16:58 +0800

    增加文章检索功能

--- routes/index.js
+++ routes/index.js
@@ -374,6 +374,23 @@ router.get('/tags/:tag', function (req, res) {
     });
 });
 
+// 搜索
+router.get('/search', function (req, res) {
+    Post.search(req.query.keyword, function(err, posts) {
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('/');
+        }
+        res.render('search', {
+            title: 'SEARCH:' + req.query.keyword,
+            posts: posts,
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
+    });
+});
+
 // 登录检查
 function checkLogin(req, res, next) {
     if (!req.session.user) {
